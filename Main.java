744a67808f5236d7631d9b67fcd60b383eb02dfb
mnguyen175 <maryannguyen2017@gmail.com>
2025-10-23 17:49:36 +0000

    code

--- Main.java
+++ Main.java
@@ -5,5 +5,45 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+
+    // Problem 2 
+    int counter = 1; 
+    for (int i = 17; i <= 73; i++)
+    {
+      System.out.print(i + " "); 
+
+      if (counter == 10)
+      {
+        System.out.println();
+        counter = 0;
+      }
+      counter++; 
+    }
+    System.out.println();
+
+    // Problem 3 
+    Scanner sc = new Scanner(System.in); 
+
+    int count = 1; 
+    System.out.println("Enter a number between 0 and 50:");
+    int num = sc.nextInt();
+    if (num <= 0 || num >= 50)
+    {
+      System.out.println("error");
+    }
+    else 
+    {
+      for (int i = num; i <= 50; i++)
+      {
+        System.out.print(i + " ");
+
+        if (count == 5)
+        {
+          System.out.println(); 
+          count = 0; 
+        }
+        count++;
+      }
+    }
   }
 }
